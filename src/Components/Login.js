8968d36b192a406abe8a9fea721d12e6de046e5d
Dev G <gangwardev50@gmail.com>
2022-09-22 10:17:15 +0530

    getting login component data

--- src/Components/Login.js
+++ src/Components/Login.js
@@ -1,8 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import { Grid, Box, TextField, Button, form,Card, Typography } from "@mui/material";
 import Pic from "./images/pic-1.png";
 
 export default function Login() {
+  const[name, setName] = useState("")
+  const[email,setEmail] = useState("")
+  const[password, setPassword] = useState("")
+
+  const handleLogin = (e) =>{
+    e.preventDefault();
+    const data = {name,email,password};
+    console.log(data);
+
+  }
   return (
     <>
       <Grid container sx={{ height: "90vh" }}>
@@ -23,11 +33,11 @@ export default function Login() {
           <Card sx={{width:'100%', height:'100%'}}>
             <Typography textAlign='center' variant='h4' fontFamily='san-serif' sx={{mt:2}}>Login Here </Typography>
           <Box component='form' noValidate sx={{mt:2, px:3}} id='login-form'>
-            <TextField noValidate required name='Name' fullWidth label='Enter Your Name' id='your-name'/>
-            <TextField noValidate required fullWidth margin='normal' sx={{mt:2}} name='email' label='Enter Your email'/>
-            <TextField   noValidate required fullWidth margin='normal' name='password' label='Your Password'/>
+            <TextField noValidate required name='Name' fullWidth label='Enter Your Name' onChange={(e)=>setName(e.target.value)} id='your-name'/>
+            <TextField noValidate required fullWidth margin='normal' sx={{mt:2}} name='email' onChange={(e)=>setEmail(e.target.value)} label='Enter Your email'/>
+            <TextField   noValidate required fullWidth margin='normal' type='password' name='password' onChange={(e)=>setPassword(e.target.value)} label='Your Password'/>
             <Box textAlign='center'>
-                <Button type='submit' variant="contained" sx={{mt:3, mb:2, px:2}}>Login</Button>
+                <Button type='submit' variant="contained" sx={{mt:3, mb:2, px:2}} onClick={handleLogin}>Login</Button>
             </Box>
           </Box>
           </Card>
--- src/Components/Logout.js
+++ src/Components/Logout.js
@@ -1,8 +1,25 @@
-import React from "react";
-import { Grid, Box, Card, TextField, Typography, Button, form } from "@mui/material";
+import React, { useState } from "react";
+import {
+  Grid,
+  Box,
+  Card,
+  TextField,
+  Typography,
+  Button,
+  form,
+} from "@mui/material";
 import Pic from "./images/pic-3.png";
 
 export default function Logout() {
+  const [name, setName] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+  const [cnfpassword, setCnfPassword] = useState(" ");
+
+  const signUp = () => {
+    let item = { name, email, password, cnfpassword };
+    console.log(item);
+  };
   return (
     <>
       <Grid container sx={{ height: "90vh" }}>
@@ -17,17 +34,67 @@ export default function Logout() {
             backgroundSize: "cover",
           }}
         ></Grid>
-        <Grid textAlign='right' item lg={6} sm={5}>
-        <Card sx={{width:'100%', height:'100%', py:2}}>
-            <Typography textAlign='center' variant='h4'>Signup</Typography>
-            <Box container='form' id='signup-form' noValidate sx={{mt:2, px:3}}>
-                <TextField noValidate required name='First Name' label='First Name' fullWidth/>
-                <TextField noValidate required name='email' sx={{mt:2}} label=' Email' fullWidth/>
-                <TextField noValidate required name='password' sx={{mt:2}} label=' Password' fullWidth/>
-                <TextField noValidate required name='confirm-password' sx={{mt:2}} label='Confirm Password' fullWidth/>
-                <Box textAlign='center'>
-                    <Button variant='contained' sx={{mt:3, mb:2, px:2}}>Signup</Button>
-                </Box>
+        <Grid textAlign="right" item lg={6} sm={5}>
+          <Card sx={{ width: "100%", height: "100%", py: 2 }}>
+            <Typography textAlign="center" variant="h4">
+              Signup
+            </Typography>
+            <Box
+              container="form"
+              id="signup-form"
+              noValidate
+              sx={{ mt: 2, px: 3 }}
+            >
+              <TextField
+                noValidate
+                required
+                name="First Name"
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                label="First Name"
+                fullWidth
+              />
+              <TextField
+                noValidate
+                required
+                name="email"
+                sx={{ mt: 2 }}
+                value={email}
+                onChange={(e) => setEmail(e.target.value)}
+                label=" Email"
+                fullWidth
+              />
+              <TextField
+                noValidate
+                required
+                name="password"
+                sx={{ mt: 2 }}
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                label="Password"
+                fullWidth
+              />
+              <TextField
+                noValidate
+                required
+                name="confirm-password"
+                type="password"
+                sx={{ mt: 2 }}
+                value={cnfpassword}
+                onChange={(e) => setCnfPassword(e.target.value)}
+                label="Confirm Password"
+                fullWidth
+              />
+              <Box textAlign="center">
+                <Button
+                  variant="contained"
+                  onClick={signUp}
+                  sx={{ mt: 3, mb: 2, px: 2 }}
+                >
+                  Signup
+                </Button>
+              </Box>
             </Box>
           </Card>
         </Grid>
